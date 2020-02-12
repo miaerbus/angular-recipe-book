f727e6c5c258af9b7819f59029ad869054d6fe33
Mia Erbus <miaerbus@Mias-MacBook-Pro.local>
2020-02-12 17:16:11 +0100

    Create separate Recipes module

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { RecipesComponent } from './recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeNotFoundComponent } from './recipe-not-found/recipe-not-found.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeNotFoundComponent,
+    RecipeEditComponent
+  ],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
+  exports: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeNotFoundComponent,
+    RecipeEditComponent
+  ]
+})
+export class RecipesModule {}
